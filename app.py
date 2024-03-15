90a4f4b39106cdb601a107239eea4c7d5838967e
DHARMIK  GANGANI <dharmikgangani786@gmail.com>
2024-03-15 19:10:54 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+  st.set_page_config(page_title='ATS Scanner', page_icon=':technologist:', layout='wide')
+  st.title('ATS Tracker Tool')
+
+
+if __name__ == '__main__':
+  main()
